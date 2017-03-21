044d946036ae8721b89c20a59c0b9e8671039f41
Baptiste Perrin <perrin.baptiste@yahoo.fr>
2017-03-21 13:04:13 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,244 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  puts "Creating exercises"
+
+puts "."
+  Exerise.create(
+  name: "Squats sautés",
+  exercise_type: "Cardio",
+  equipment: "Aucun"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Montées de genous",
+  exercise_type: "Cardio",
+  equipment: "Aucun"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Montée 2 genous joins",
+  exercise_type: "Cardio",
+  equipment: "Aucun"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Mountain climbers",
+  exercise_type: "Cardio",
+  equipment: "Aucun"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Fentes sautées",
+  exercise_type: "Cardio",
+  equipment: "Aucun"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Corde à sauter",
+  exercise_type: "Cardio",
+  equipment: "Corde à sauter"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Montées 2 genous mains au sol",
+  exercise_type: "Cardio",
+  equipment: "Aucun"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Fentes sautées sur chaise",
+  exercise_type: "Cardio",
+  equipment: "Chaise/banc"
+  )
+
+puts "."
+ Exercise.create(
+  name: "Gainage abdos sur le dos",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Rouleau",
+  exercise_type: "Abdos",
+  equipment: "Rouleau"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Abdos complets",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Velo",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Touchés pieds à la verticale",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Ciseaux",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Complets avec mains au sol",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Mountain climbers croisés (sans vitese)",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Position assise, toucher gauche/groite",
+  exercise_type: "Abdos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Pompes déclinées",
+  exercise_type: "Pecs",
+  equipment: "Chaise/banc"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Pompes mains serrées sur les genous",
+  exercise_type: "Pecs",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Pompes",
+  exercise_type: "Pecs",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Pompes TRX",
+  exercise_type: "Pecs",
+  equipment: "TRX"
+ )
+
+ Exercise.new(
+  ,
+  exercise_type: "Pecs",
+  equipment: "Aucun",  )
+
+puts "."
+ Exercise.create(
+  name: "I au TRX",
+  exercise_type: "Dos",
+  equipment: "TRX"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Y au TRX",
+  exercise_type: "Dos",
+  equipment: "TRX"
+ )
+
+puts "."
+ Exercise.create(
+  name: "T au TRX",
+  exercise_type: "Dos",
+  equipment: "TRX"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Relevé de buste avec les coudes",
+  exercise_type: "Dos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Tractions",
+  exercise_type: "Dos",
+  equipment: "Barre de tractions"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Reverse snow angels",
+  exercise_type: "Dos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Superman",
+  exercise_type: "Dos",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Butterfly aux haltères",
+  exercise_type: "Dos",
+  equipment: "Haltères"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Rings-row",
+  exercise_type: "Dos",
+  equipment: "TRX"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Pompes épaules",
+  exercise_type: "Epaules",
+  equipment: "Aucun"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Développé épaules",
+  exercise_type: "Epaules",
+  equipment: "Haltères"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Dips",
+  exercise_type: "Tris",
+  equipment: "Chaise/banc"
+ )
+
+puts "."
+ Exercise.create(
+  name: "Plank push-up",
+  exercise_type: "Pecs/abdos",
+  equipment: "Aucun"
+ )
