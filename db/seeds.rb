a86564b3bf2c7a78d48556640b1795af022c640f
Baptiste Perrin <perrin.baptiste@yahoo.fr>
2017-04-28 19:14:44 +0200

    added exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,246 +1,451 @@
-  puts "Creating exercises"
+puts "Creating exercises"
 
-puts "."
-  Exercise.create(
-  name: "Squats sautés",
-  exercise_type: "Cardio",
-  equipment: "Aucun"
-  )
-
-puts "."
- Exercise.create(
-  name: "Montées de genous",
-  exercise_type: "Cardio",
-  equipment: "Aucun"
-  )
-
-puts "."
- Exercise.create(
-  name: "Montée 2 genous joins",
-  exercise_type: "Cardio",
-  equipment: "Aucun"
-  )
-
-puts "."
- Exercise.create(
-  name: "Mountain climbers",
-  exercise_type: "Cardio",
-  equipment: "Aucun"
-  )
-
-puts "."
- Exercise.create(
-  name: "Fentes sautées",
-  exercise_type: "Cardio",
-  equipment: "Aucun"
-  )
-
-puts "."
- Exercise.create(
-  name: "Corde à sauter",
-  exercise_type: "Cardio",
-  equipment: "Corde à sauter"
-  )
-
-puts "."
- Exercise.create(
-  name: "Montées 2 genous mains au sol",
-  exercise_type: "Cardio",
-  equipment: "Aucun"
-  )
-
-puts "."
- Exercise.create(
-  name: "Fentes sautées sur chaise",
-  exercise_type: "Cardio",
-  equipment: "Chaise/banc"
-  )
-
-puts "."
- Exercise.create(
-  name: "Gainage abdos sur le dos",
-  exercise_type: "Abdos",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Rouleau",
-  exercise_type: "Abdos",
-  equipment: "Rouleau"
- )
-
-puts "."
- Exercise.create(
+Exercise.create(
   name: "Abdos complets",
   exercise_type: "Abdos",
-  equipment: "Aucun"
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Allongé sur le dos, remonter le buste et les genoux jusqu'à une position accroupie. Enchainer de manière dynamique.">,
  )
 
-puts "."
- Exercise.create(
-  name: "Velo",
-  exercise_type: "Abdos",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Touchés pieds à la verticale",
-  exercise_type: "Abdos",
-  equipment: "Aucun"
- )
-
-puts "."
+ puts "."
  Exercise.create(
   name: "Ciseaux",
   exercise_type: "Abdos",
-  equipment: "Aucun"
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Position assise jambes tendues, croiser les jambes en alternant">,
  )
 
-puts "."
+ puts "."
  Exercise.create(
   name: "Complets avec mains au sol",
   exercise_type: "Abdos",
-  equipment: "Aucun"
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Position assise avec mains au sol et jambes tendues, plier les jambes en remontant le buste puis les retendre les jambes en descendant le buste.">,
  )
 
-puts "."
+ puts "."
  Exercise.create(
-  name: "Mountain climbers croisés (sans vitese)",
-  exercise_type: "Abdos",
-  equipment: "Aucun"
+  name: "Corde à sauter",
+  exercise_type: "Cardio",
+  equipment: "Corde à sauter",
+  initial_status: nil,
+  available: nil,
+  description: "">,
  )
 
-puts "."
- Exercise.create(
-  name: "Position assise, toucher gauche/groite",
-  exercise_type: "Abdos",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Pompes déclinées",
-  exercise_type: "Pecs",
-  equipment: "Chaise/banc"
- )
-
-puts "."
- Exercise.create(
-  name: "Pompes mains serrées sur les genous",
-  exercise_type: "Pecs",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Pompes",
-  exercise_type: "Pecs",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Pompes TRX",
-  exercise_type: "Pecs",
-  equipment: "TRX"
- )
-
-puts "."
- Exercise.create(
-  name: "Pompes spider-man",
-  exercise_type: "Pecs",
-  equipment: "Aucun"
-)
-
-puts "."
- Exercise.create(
-  name: "I au TRX",
-  exercise_type: "Dos",
-  equipment: "TRX"
- )
-
-puts "."
- Exercise.create(
-  name: "Y au TRX",
-  exercise_type: "Dos",
-  equipment: "TRX"
- )
-
-puts "."
- Exercise.create(
-  name: "T au TRX",
-  exercise_type: "Dos",
-  equipment: "TRX"
- )
-
-puts "."
- Exercise.create(
-  name: "Relevé de buste avec les coudes",
-  exercise_type: "Dos",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Tractions",
-  exercise_type: "Dos",
-  equipment: "Barre de tractions"
- )
-
-puts "."
- Exercise.create(
-  name: "Reverse snow angels",
-  exercise_type: "Dos",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Superman",
-  exercise_type: "Dos",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Butterfly aux haltères",
-  exercise_type: "Dos",
-  equipment: "Haltères"
- )
-
-puts "."
- Exercise.create(
-  name: "Rings-row",
-  exercise_type: "Dos",
-  equipment: "TRX"
- )
-
-puts "."
- Exercise.create(
-  name: "Pompes épaules",
-  exercise_type: "Epaules",
-  equipment: "Aucun"
- )
-
-puts "."
- Exercise.create(
-  name: "Développé épaules",
-  exercise_type: "Epaules",
-  equipment: "Haltères"
- )
-
-puts "."
+ puts "."
  Exercise.create(
   name: "Dips",
   exercise_type: "Tris",
-  equipment: "Chaise/banc"
+  equipment: "Chaise/banc",
+  initial_status: nil,
+  available: nil,
+  description: "Mains sur une chaise, fesses dans le vide et jambes tendues, alterner bras pliés / bras tendus">,
  )
 
-puts "."
+ puts "."
+ Exercise.create(
+  name: "Développé épaules avec haltères",
+  exercise_type: "Epaules",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description: "Bras écartés, coudes à 90°, remonter les altères au dessus de la tête et enchainer">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Elévations frontales avec haltères",
+  exercise_type: "Epaules",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description: "Debout bras tendus le long du corps, remonter une altère face à soi bras tendu puis alterner">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Elévations latérales avec haltères",
+  exercise_type: "Epaules",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description: "Bras semi-fléchis le long du corps, ramener les bras à hauteur des épaules en gardant les coudes au dessus des altères">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Fentes sautées",
+  exercise_type: "Cardio",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Enchainer les fentes jambe gauche/droite de manière dynamique">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Fentes sautées sur chaise",
+  exercise_type: "Cardio",
+  equipment: "Chaise/banc",
+  initial_status: nil,
+  available: nil,
+  description:
+   "1 pied sur le sol, l'autre sur une chaise, remonter le plus haut possible au dessus de la chaise en alternant la jambe de poussée.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Gainage abdos position assise",
+  exercise_type: "Abdos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Assis sur le sol, descendre le buste le plus bas possible sans décoller les pieds. Tenir la position.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Gainage dorsal",
+  exercise_type: "Dos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Sur le dos, coudes au sol et jambes tendues, tenir la position en essayant de pousser le ventre le plus haut possible">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "I au TRX",
+  exercise_type: "Epaules",
+  equipment: "TRX",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Position de départ bras tendus face à vous, remonter les bras serrés à la verticale (position I). Gérer la difficulté en avançant/reculant les pieds.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "L'oiseau avec haltères",
+  exercise_type: "Dos",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Jambes semi-fléchies, poitrine face au sol et altères sous la poitrine, écarter les bras jusqu'à les ramener parallèles au sol ">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Montée 2 genoux joints",
+  exercise_type: "Cardio",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Sur place, remonter en même temps les 2 genoux. Enchainer le plus vite possible">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Montées 2 genous mains au sol",
+  exercise_type: "Cardio",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Départ position gainage bras tendus. Ramener les pieds au niveau de la poitrine puis revenir en position de départ">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Montées de genous",
+  exercise_type: "Cardio",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Sur place, monter les genoux le plus vite et le plus haut possible">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Mountain climbers",
+  exercise_type: "Cardio",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Faire des montées de genoux avec les mains au sol">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Mountain climbers croisés (sans vitesse)",
+  exercise_type: "Abdos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Position gainage bras tendus. Ramener le genoux au niveau du coude opposé puis revenir en position de départ et alterner">,
+ )
+
+ puts "."
  Exercise.create(
   name: "Plank push-up",
-  exercise_type: "Pecs/abdos",
-  equipment: "Aucun"
+  exercise_type: "Tris",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Position gainage mains au sol, remonter jusqu'à avoir les 2 bras tendus puis redescendre et recommencer.">,
  )
+
+ puts "."
+ Exercise.create(
+  name: "Pompes TRX",
+  exercise_type: "Pecs",
+  equipment: "TRX",
+  initial_status: nil,
+  available: nil,
+  description: "Pompes pieds au sol et mains dans le TRX">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Pompes classiques",
+  exercise_type: "Pecs",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Mains largeur d'épaules, descendre en gardant les coudes le long du corps">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Pompes déclinées",
+  exercise_type: "Pecs",
+  equipment: "Chaise/banc",
+  initial_status: nil,
+  available: nil,
+  description: "Pompes avec pieds sur une chaise/banc">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Pompes mains serrées sur les genous",
+  exercise_type: "Pecs",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Genoux au sol, mains serrées sous la poitrine, creuser le dos avant de tendre les bras.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Pompes mains écartées",
+  exercise_type: "Pecs",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Mains à 1,5x la largeur des épaules, descendre avec les coudes légèrement orientés vers l'extérieur">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Pompes spider-man",
+  exercise_type: "Pecs",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Pompes. Ramener le genoux au niveau du coude à la descente">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Pompes épaules",
+  exercise_type: "Epaules",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Pompes avec le corps en V inversé, fesses le plus haut possible">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Reverse snow angels",
+  exercise_type: "Dos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Allongé sur le ventre,  buste relevé et bras tendus le long du corps. Ramener les bras au dessus de la tête en les gardant tendus et les mains le plus haut possible.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Rings-rows au TRX",
+  exercise_type: "Dos",
+  equipment: "TRX",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Pieds au sol et bras tendus face à soi, ramener les mains au niveau des côtes en tirant sur les coudes. Gérer la difficulté en avançant/reculant les pieds.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Rotations de buste",
+  exercise_type: "Abdos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Position assise, toucher le plus loin possible derrière soi de chaque coté (avec ou sans altère)">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Rouleau",
+  exercise_type: "Abdos",
+  equipment: "Rouleau",
+  initial_status: nil,
+  available: nil,
+  description: nil>,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Rowing 2 althères",
+  exercise_type: "Dos",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Jambes légèrement fléchies, buste en avant, et bras tendus vers le sol, remonter les altères en tirant sur les coudes (les coudes restent le long du corps)">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Sit-ups avec haltère",
+  exercise_type: "Abdos",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description: "Position allongée jambes fléchies et haltère sur le ventre, remonter le buste avec l'haltère au dessus de soi">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Squats sautés",
+  exercise_type: "Cardio",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Fléchir les jambes jusqu'à 90° et remonter en sautant">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Statique avec élévation d'haltère",
+  exercise_type: "Abdos",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Position assise avec pieds au sol, descendre le buste le plus bas possible. Remonter l'haltère à la verticale puis la ramener à la poitrine en tenant la position">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Superman",
+  exercise_type: "Pecs/abdos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Allongé sur le ventre, bras tendus à la verticale, lever le buste et les jambes puis revenir en position de départ">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "T au TRX",
+  exercise_type: "Dos",
+  equipment: "TRX",
+  initial_status: nil,
+  available: nil,
+  description: "Position de départ bras tendus face à vous. Ecarter les bras jusqu'à former un T puis revenir en position de départ">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Tirage menton 1 haltère",
+  exercise_type: "Epaules",
+  equipment: "Haltères",
+  initial_status: nil,
+  available: nil,
+  description: "Bras tendus le long du corps avec une haltère, remonter l'haltère au menton en gardant les coudes le plus haut possible">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Touchés pieds à la verticale",
+  exercise_type: "Abdos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Allongé sur le sol, jambes tendues à 90°, toucher les pieds/chevilles avec les mains">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Tractions en pronation",
+  exercise_type: "Dos",
+  equipment: "Barre de tractions",
+  initial_status: nil,
+  available: nil,
+  description: "Tractions mains écartées, paumes de mains vers l'avant (pouces à l'intérieur)">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Tractions en supination",
+  exercise_type: "Dos",
+  equipment: "Barre de tractions",
+  initial_status: nil,
+  available: nil,
+  description: "Tractions mains largeur d'épaules, paumes face à vous (pouces vers l'extérieur)">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Trio de pompes",
+  exercise_type: "Pecs",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description:
+   "1 tiers de pompes mains serrées, puis 1 tiers de pompes mains largeur d'épaules, puis 1 tiers de pompes mains écartées. Essayer de faire le même nombre de répétitions à chaque exercice.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Velo",
+  exercise_type: "Abdos",
+  equipment: "Aucun",
+  initial_status: nil,
+  available: nil,
+  description: "Allongé, une jambe pliée l'autre tendue, toucher le genou plié avec le coude opposé puis alterner.">,
+ )
+
+ puts "."
+ Exercise.create(
+  name: "Y au TRX",
+  exercise_type: "Epaules",
+  equipment: "TRX",
+  initial_status: nil,
+  available: nil,
+  description:
+   "Position de départ bras tendus face à vous, remonter les bras à la verticale en les écartant à 45° (position Y). Gérer la difficulté en avançant/reculant les pieds."
+)
